115cf6b57cad130dfedc6509738989cd5f70861a
duckie <lucascoliado@hotmail.com>
2017-10-30 17:10:32 -0200

    Tweak comments in Overlapping Rectangles Python Solution

--- School Levels/Overlapping Rectangles/Python/run.py
+++ School Levels/Overlapping Rectangles/Python/run.py
@@ -9,7 +9,7 @@
 # any of the following conditions:
 #  - Completely to the Right of R2 (R1.x1 > R2.x2)(C1)*
 #  - Completely to the Left of R2 (R1.x2 < R2.x1)(C2)*
-#  - Completely Above of R2 (R1.y2 > R2.y1)(C3)*
+#  - Completely Above R2 (R1.y2 > R2.y1)(C3)*
 #  - Completely Under R2 (R1.y1 < R2.y2)(C4)*
 # Therefore, R1 *is* overlapping R2 is a true statement if and only if ~(C1 v C2 v C3 v C4)
 
@@ -22,14 +22,14 @@ class Rectangle:
         self.bottomRightX=int(args[2])
         self.bottomRightY=int(args[3])
 
-    #Each of these methods return True if the position with regard of the input rectangle is met, False if otherwise
+    #Each of these methods return True if the position with regard to the input rectangle is met, False if otherwise
     def isToRightOf(self, otherRectangle):
         return self.topLeftX > otherRectangle.bottomRightX
 
     def isToLeftOf(self, otherRectangle):
         return self.bottomRightX < otherRectangle.topLeftX
 
-    def isAboveOf(self, otherRectangle):
+    def isAbove(self, otherRectangle):
         return self.bottomRightY > otherRectangle.topLeftY
 
     def isUnder(self, otherRectangle):
